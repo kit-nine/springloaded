d68aad9c98de63a2d19166fde115b292eb506a5a
kit <kit_nine@outlook.com>
2023-11-06 11:48:01 -0500

    camera movement and colliders

---
+++ Assets/Assets/CameraFollow.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset = new Vector3(0,2,-10);
+    public float smoothTime = 0.25f;
+    Vector3 currentVelocity;
+    // update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = Vector3.SmoothDamp(transform.position, target.position + offset, ref currentVelocity, smoothTime);
+    }
+}
