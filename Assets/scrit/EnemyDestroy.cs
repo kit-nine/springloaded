f9bf4a352b1eb75e5a9af200118eef226f07a5e7
kit <kit_nine@outlook.com>
2023-12-14 12:39:37 -0500

    push!

--- Assets/scrit/EnemyDestroy.cs
+++ Assets/scrit/EnemyDestroy.cs
@@ -2,11 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyDestroy : MonoBehaviour
-{
-    private void OnTriggerEnter2D(Collider2D collision){
-        if(collision.gameObject.tag == "Enemy"){
-            Destroy(collision.gameObject);
+public class EnemyDestroy : MonoBehaviour {
+    private Animator enemyAnimator;
+    public Rigidbody2D rb;
+    public float bounce;
+    void Start() {
+        enemyAnimator = transform.parent.GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void OnCollisionEnter2D(Collision2D collision){
+        if(collision.gameObject.tag == "Player"){
+            rb.velocity = new Vector2(rb.velocity.x, bounce);
+            enemyAnimator.SetTrigger("Destroy");
+            Invoke("DestroyEnemy", 0.5f);
         }
+    }
+    public void DestroyEnemy() {
+        Destroy(transform.parent.gameObject);
     }
 }
---
+++ Assets/scrit/playerrespawn.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class playerrespawn : MonoBehaviour{
+
+
+    private void OnCollisionEnter2D(Collision2D collision){
+        if (collision.gameObject.CompareTag("Enemy")){
+            RestartLevel();
+        }
+    }
+    private void RestartLevel(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
